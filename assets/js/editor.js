5c824031012664474735f946bfcf3ba0b1c873ba
Van Lyubov <33548464+dalpan@users.noreply.github.com>
2021-04-07 21:44:49 +0700

    Add files via upload

---
+++ assets/js/editor.js
@@ -0,0 +1,32 @@
+
+// Munculkan Code Di tag iframe area
+function RunCode(){
+    		var CodeText = document.getElementById('TextEditor').value;
+    		document.getElementById('result').srcdoc = CodeText;
+}
+
+// Ubah string textarea menjadi Base64_encode
+function Encbase64(){
+    		var Code   = document.getElementById('TextEditor').value;
+    		if(Code == ''){
+    			alert('Harap Isi Bidang Jangan Biarkan Kosong');
+    		}
+    		var base64 = btoa(Code);	document.getElementById('TextEditor').value = base64;
+}
+
+// Sebaliknya
+function Decbase64(){
+	var Code   = document.getElementById('TextEditor').value;
+	if(Code == ''){
+    			alert('Harap Isi Bidang Jangan Biarkan Kosong');
+    		}
+	var Unicode = atob(Code);
+	document.getElementById('TextEditor').value = Unicode;
+}
+
+// Salin Code Di Textarea
+function CopyCode(){
+    		var copeh = document.getElementById("TextEditor").select();
+			document.execCommand("copy");
+			alert("Sukses");
+}
